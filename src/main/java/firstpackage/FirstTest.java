e0a48b651a38716876a0482286266068f5efad96
Daniel Junior <jrzabott@gmail.com>
2023-02-18 10:14:46 +0000

    most of changes required to test a logging using a runtime added appender and checking the results.

---
+++ src/main/java/firstpackage/FirstTest.java
@@ -0,0 +1,11 @@
+public class FirstTest {
+    public static void main(String[] args) {
+        FirstTest ft = new FirstTest();
+        final String letterA = ft.returnLetterA();
+        System.out.println(letterA);
+    }
+
+    String returnLetterA() {
+        return "A";
+    }
+}
